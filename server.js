dfeea8573504e940ea7c5e7694fe9780becae7cb
halwilcox <halwilcox09@gmail.com>
2021-09-28 11:55:49 -0600

    init

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const Rollbar = require('rollbar')
+
+const rollbar = new Rollbar({
+    accessToken: '06be4301e0694c2b87e31334686391d3',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './client/index.html'))
+    rollbar.info('Html was monitored successfully!')
 })
 
 const port = process.env.PORT || 5656
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log( `Hippity Hoppity your server is poppening on port: ${port}`))
